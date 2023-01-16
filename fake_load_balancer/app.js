ae00d599951c3a340805b6e831eba72ca1c37746
Toluwase Afolayan <tolu.afolayan27@gmail.com>
2023-01-16 15:21:54 -0500

    added code for checking shards

--- fake_load_balancer/app.js
+++ fake_load_balancer/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const port = 3000
 
@@ -16,12 +17,29 @@ const shards = [
     0
 ]
 
+const check_shards = () => {
+    // get num shards, and shard statuses
+
+    axios({
+        method: 'get',
+        url: 'http://localhost:5000/'
+    }).then((resp) => {
+        let num_shards = resp.data.server.num_shards;
+        let shard_statuses = resp.data.shard_statuses;
+        return num_shards, shard_statuses
+    }).catch((err) => {
+        console.log(`Error: ${err}`);
+    })
+
+    
+}
 
   let shard_avail = () => {
-  for (i = 0; i < (shards.length); i++){
-    if (shards[i] === 0){
+    const [num_shards, shard_statuses] = check_shards();
+  for (i = 0; i < (shard_statuses.length); i++){
+    if (shard_statuses[i] === 0){
         console.log("Available and online")
-    } else if (shards[i] === 2){
+    } else if (shard_statuses[i] === 2){
         console.log("Not available because it is offline")
     } else {console.log("This will be open in a second")}
 
@@ -30,7 +48,7 @@ const shards = [
 
 
 app.get('/', (req, res) => {
-  shard_avail()
+    shard_avail()
     res.send('Load Balancer!')
 })
 
