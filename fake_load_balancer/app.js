1865b2eba37c51b26379c1f4e30b83258d230342
Yves Oubida <yvesoubida@gmail.com>
2023-01-15 19:33:23 -0500

    Created example servers and log server availability

--- fake_load_balancer/app.js
+++ fake_load_balancer/app.js
@@ -2,6 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const shards = [
+"http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:3002"
+]
+
+  
+  let shard_avail = () => {
+  for (i = 0; i < (shards.length - 1); i++){
+    if (shards[i].status === "online"){
+            console.log("Available")
+    } else if (shards[i].status === "offline"){
+            console.log("Not available")
+    } else console.log("This will be open in a second")
+
+  }
+}
+    
 app.get('/', (req, res) => {
     res.send('Load Balancer!')
 })
