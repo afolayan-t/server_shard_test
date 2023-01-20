6c0bc3f483f91278cbef20c58672945e2141a14a
Toluwase Afolayan <tolu.afolayan27@gmail.com>
2023-01-20 17:28:11 -0500

    fixed issue with receiving response with axios

--- fake_load_balancer/app.js
+++ fake_load_balancer/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const axios = require('axios')
+const { response } = require('express')
 const app = express()
 const port = 3000
 
-//Get shard_Id lists into shard array
+// Get shard_id lists into shard array
 const shards = [
     0,
     0,
@@ -17,39 +18,46 @@ const shards = [
     0
 ]
 
-const check_shards = () => {
+async function check_shards() {
     // get num shards, and shard statuses
+    // let num_shards;
+    // let shard_statuses;
 
-    axios({
+    resp = await axios({
         method: 'get',
         url: 'http://localhost:5000/'
     }).then((resp) => {
-        let num_shards = resp.data.server.num_shards;
-        let shard_statuses = resp.data.shard_statuses;
-        return num_shards, shard_statuses
+        // num_shards = resp.data.server.num_shards;
+        // shard_statuses = resp.data.shard_statuses;
+        // console.log(first)
+        console.log(resp)
+        return resp
     }).catch((err) => {
         console.log(`Error: ${err}`);
     })
-
-    
+    return resp.data
+    // return num_shards, shard_statuses
 }
 
-  let shard_avail = () => {
-    const [num_shards, shard_statuses] = check_shards();
-  for (i = 0; i < (shard_statuses.length); i++){
+//   let shard_avail = () => {
+//     const response = check_shards();
+//     console.log
+
+/*   for (i = 0; i < (shard_statuses.length); i++){
     if (shard_statuses[i] === 0){
         console.log("Available and online")
     } else if (shard_statuses[i] === 2){
         console.log("Not available because it is offline")
     } else {console.log("This will be open in a second")}
 
-  }
-}
+  } */
+// }
 
-
-app.get('/', (req, res) => {
-    shard_avail()
+app.get('/', async (req, res) => {
+    // shard_avail()
     res.send('Load Balancer!')
+    let data = await check_shards()
+    console.log(data)
 })
 
 app.listen(port, () => {
