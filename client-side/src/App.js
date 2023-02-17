4ed372bdbaab871e265f7835c09578cef736a7e0
Yves Oubida <yvesoubida@gmail.com>
2023-02-16 19:55:59 -0500

    Added a Number Generator Button

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,20 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-
+//Get Function
 function Get_Func() {
-  fetch(document.getElementById("search").value) 
-    .then((res) =>
-    res.json()
-    )
-    .catch((error) =>
-  console.log(error));
+  document.getElementById("ActG").action = document.getElementById("search").value;
+  document.getElementById("get-btn").click();
+
   }
- // document.getElementById("ActG").action = document.getElementById("search").value; 
+ 
+function input(){
+  if(document.getElementsByClassName("btn").value === "option 1"){
+    Get_Func()
+  } else{Post_Func()}
+}
+ //Trying to get the search bar to complete an event click handler
+/*document.getElementById("search").addEventListener('keypress', function(e){
+  if (e.key === 'Enter') {
+   Post_Func()
+  }
+});
+
+document.getElementById("search").EventListener('keypress', function(e){
+  if (e.key === 'Enter') {
+   Post_Func()
+  }
+});*/
+//Change the event listener to redirect based on clicking the get button
+//document.getElementById("get-btn").onclick = function() {Get_Func()};
+//Or redirect based pressing enter on the search bar
+
+
+//Post Function
 function Post_Func() {
   document.getElementById("ActP").action = document.getElementById("search").value;
+  document.getElementById("post-btn").click();
 } 
 
+//Talk with Back-End to request number
+//Ex: send Job Post request
+function Num_Gen(){
+  axios.post('http://127.0.0.1:5000/1/send_job')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
 function App() {
   return (
     <div className="App">
@@ -27,15 +60,25 @@ function App() {
         >
           Made with React
         </a>
-      <header className="App-header">
-          <form id="ActG" action="" method="GET"> 
-            <button onClick={Get_Func} className="btn" id="get-btn">GET Button</button> 
-          </form>
-          <form id="ActP" action="" method="POST">
-            <button onClick={Post_Func} className="btn" id="post-btn">POST Button</button>
-          </form>
-        <input id="search" type="text"></input>
-      </header>
+  <header className="App-header">
+    <h1 className="Head">Enter the URL and decide between a Get or Post Method:</h1>
+    <div id ="buttons">
+      <select name="selectList" className="btn">
+        <option value="option 1">
+            <form id="ActG" action="" method="GET"> 
+              <button onClick={Get_Func} id="get-btn">GET Button</button> 
+            </form>
+        </option>
+        <option value="option 2">
+            <form id="ActP" action="" method="POST">
+              <button onClick={Post_Func} id="post-btn">POST Button</button>
+            </form>
+        </option>
+      </select>
+      <button className="btn" onClick={Num_Gen}>Get a random number</button>
+    </div>
+      <input id="search" type="text" onKeyPress={input}></input>
+  </header>
     </div>
   );
 }
